3f130fe8c5518d9a10d23189c56fc48c9619fda5
Leonardo Ramirez <ldramirez@uce.edu.ec>
2024-02-27 18:36:44 -0600

    Taller 39

--- src/main/java/com/uce/edu/controller/PersonaController.java
+++ src/main/java/com/uce/edu/controller/PersonaController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,43 +20,62 @@ import com.uce.edu.service.IPersonaService;
 @Controller
 @RequestMapping("/personas")
 public class PersonaController {
-	
+
 	@Autowired
 	private IPersonaService iPersonaService;
-	
-	//Diferentes tipos de request
-	//Verbos o metodos HTTP
-	
-	//Path
-	//GET
+
+	// Diferentes tipos de request
+	// Verbos o metodos HTTP
+
+	// Path
+	// GET
 	@GetMapping("/buscarTodos")
 	public String buscarTodos(Model modelo) {
-		
-		List<Persona> lista=this.iPersonaService.buscarTodos();
-		modelo.addAttribute("personas",lista);
+
+		List<Persona> lista = this.iPersonaService.buscarTodos();
+		modelo.addAttribute("personas", lista);
 		return "VistaListaPersonas";
 	}
-	@GetMapping("/buscar")
-	public String buscarPorCedula(String cedula) {
-		
-		return "";
+
+	@GetMapping("/buscarPorCedula/{cedulaPersona}")
+	public String buscarPorCedula(@PathVariable("cedulaPersona") String cedula, Model modelo) {
+		Persona persona = this.iPersonaService.buscarPorCedula(cedula);
+		modelo.addAttribute("persona", persona);
+		return "vistaPersona";
 	}
-	@PutMapping("/actualizar")
-	public String actualizar() {
-		
-		return "";
+
+	@PutMapping("/actualizar/{cedulaPersona}")
+	public String actualizar(@PathVariable("cedulaPersona") String cedula, Persona persona) { // persona es el modelo
+		persona.setCedula(cedula);
+		Persona perAux = this.iPersonaService.buscarPorCedula(cedula);
+		perAux.setNombre(persona.getNombre());
+		perAux.setApellido(persona.getApellido());
+		perAux.setCedula(persona.getCedula());
+		perAux.setGenero(persona.getGenero());
+
+		this.iPersonaService.actualizar(perAux);
+		return "redirect:/personas/buscarTodos";
 	}
-    @DeleteMapping("/borrar")
-	public String borrar() {
-		
-    	return "";
+
+	@DeleteMapping("/borrar/{cedula}")
+	public String borrar(@PathVariable("cedula") String cedula ) {
+
+		this.iPersonaService.borrarPorCedula(cedula);
+		return "redirect:/personas/buscarTodos";
 	}
-    
-    @PostMapping("/guardar")
-    public String  guardar() {
-    	
-    	return "";
-    }
-    
+
+	@PostMapping("/insertar")
+	public String insertar(Persona persona) {
+		
+		this.iPersonaService.guardar(persona);
+
+		return "redirect:/personas/buscarTodos";
+	}
+	
+	@GetMapping("/nuevaPersona")
+	public String mostrarNuevaPersona(Model modelo) {
+		modelo.addAttribute("persona", new Persona());
+		return "vistaNuevaPersona";
+	}
 
 }
